95d2c24f80d5c75725cb7f3f001636aaac7d18b3
Edufgs <edu.fgs14@gmail.com>
2022-09-23 10:53:52 -0400

    add service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { HEROES } from '../mock-heroes';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes', //Esse é o nome do componente para adicionar no html do app
@@ -8,21 +9,32 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  heroes = HEROES;
+  
+  heroes: Hero[] = [];
 
   selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 
-  constructor() { }
+  constructor(
+    private heroService: HeroService, 
+    private messageService: MessageService
+  ) {} 
   /**
    * ngOnInit() é um gancho de ciclo de vida . 
    * Chamadas angulares ngOnInit()logo após a criação de um componente. 
    * É um bom lugar para colocar a lógica de inicialização.
    */
   ngOnInit(): void {
+    this.getHeroes();
+  }
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+  this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
 }
